96521f60fb631b420499a9f9142daefb82958f8c
mraznjevic <mirelaraznjevic@gmail.com>
2024-05-05 11:26:32 +0200

    Dodala komponentu za prvu stranicu (FirstPage.vue)

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Prijava</router-link>
+      <router-link to="/FirstPage">FirstPage</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import FistPage from '../views/FirstPage.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/FirstPage',
+    name: 'FirstPage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "FirstPage" */ '../views/FirstPage.vue')
+  },
   {
     path: '/',
     name: 'Home',
@@ -18,6 +28,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "Login" */ '../views/Login.vue')
   }
+
 ]
 
 const router = new VueRouter({
