6712aa699b6cb20bf9f92dd9187f8eb1ef440269
mraznjevic <mirelaraznjevic@gmail.com>
2024-06-01 16:48:23 +0200

    Dio za prikazivanje komentara dodan u SunsetMomentsCard komponentu
    Komentar se prikazuje ispod slike u aplikaciji :)

--- src/components/SunsetMomentsCard.vue
+++ src/components/SunsetMomentsCard.vue
@@ -1,4 +1,4 @@
-<template>
+<<template>
   <div class="card text-center">
     <div class="card-header">
       {{ info.description }}
@@ -8,6 +8,21 @@
     </div>
     <div class="card-footer text-muted">
       {{ postedFromNow }}
+    </div>
+
+    <!-- Prikaz postojećih komentara -->
+    <div class="comments">
+      <div v-for="(comment, index) in info.comments" :key="index" class="comment">
+        <p>{{ comment.text }}</p>
+        <span>{{ comment.user }}</span>
+        <span>{{ formatDate(comment.timestamp) }}</span>
+      </div>
+    </div>
+
+    <!-- Unos novog komentara -->
+    <div class="add-comment">
+      <input type="text" v-model="newCommentText" placeholder="Dodaj komentar..." />
+      <button @click="addNewComment">Dodaj komentar</button>
     </div>
   </div>
 </template>
@@ -22,5 +37,55 @@ export default {
       return moment(this.info.time).fromNow();
     },
   },
+  data() {
+    return {
+      newCommentText: ''
+    };
+  },
+  methods: {
+    addNewComment() {
+      if (!this.newCommentText) return;
+
+      // Emitirajte događaj s podacima postId i teksta novog komentara
+      this.$emit('add-comment', this.info.id, this.newCommentText);
+
+      // Resetirajte polje za unos novog komentara
+      this.newCommentText = '';
+    },
+    formatDate(timestamp) {
+      // Implementirajte logiku za formatiranje vremenske oznake
+    }
+  }
 };
 </script>
+
+<style scoped>
+.comments {
+  margin-top: 10px;
+}
+
+.comment {
+  margin-bottom: 10px;
+}
+
+.add-comment {
+  margin-top: 20px;
+}
+
+.add-comment input {
+  width: 70%;
+  margin-right: 10px;
+}
+
+.add-comment button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+.add-comment button:hover {
+  background-color: #0056b3;
+}
+</style>
