62c6a589b46465232c0779ffdae889ec45fe78bc
mraznjevic <mirelaraznjevic@gmail.com>
2024-06-02 11:41:17 +0200

    Uređena stilizacija za ImageDetail.vue

--- src/views/ImageDetail.vue
+++ src/views/ImageDetail.vue
@@ -1,18 +1,21 @@
 <template>
-  <div v-if="image">
+  <div v-if="image" class="image-detail">
     <h1>{{ image.description }}</h1>
-    <img :src="image.url" :alt="image.description" />
-    <div v-for="comment in image.comments" :key="comment.timestamp">
-      <p>{{ comment.text }}</p>
-      <span>{{ comment.user }}</span>
-      <span>{{ formatDate(comment.timestamp) }}</span>
+    <img :src="image.url" :alt="image.description" class="image"/>
+    <div class="comments-section">
+      <h2>Komentari</h2>
+      <div v-for="comment in image.comments" :key="comment.timestamp" class="comment">
+        <p class="comment-text">{{ comment.text }}</p>
+        <span class="comment-user">{{ comment.user }}</span>
+        <span class="comment-date">{{ formatDate(comment.timestamp) }}</span>
+      </div>
     </div>
-    <div>
-      <input type="text" v-model="newCommentText" placeholder="Dodaj komentar..." />
-      <button @click="addComment">Dodaj komentar</button>
+    <div class="add-comment">
+      <input type="text" v-model="newCommentText" placeholder="Dodaj komentar..." class="comment-input"/>
+      <button @click="addComment" class="comment-button">Dodaj komentar</button>
     </div>
   </div>
-  <div v-else>
+  <div v-else class="loading">
     <p>Učitavanje...</p>
   </div>
 </template>
@@ -20,6 +23,7 @@
 <script>
 import { db } from '@/firebase';
 import moment from 'moment';
+import store from '@/store';
 
 export default {
   data() {
@@ -67,31 +71,80 @@ export default {
 };
 </script>
 
-
-
 <style scoped>
+.image-detail {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+}
+
+.image {
+  max-width: 100%;
+  height: auto;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+
+.comments-section {
+  margin-top: 30px;
+}
+
+.comments-section h2 {
+  margin-bottom: 15px;
+  font-size: 1.5em;
+}
+
 .comment {
+  background-color: #f9f9f9;
+  padding: 10px;
   margin-bottom: 10px;
+  border-radius: 5px;
+  text-align: left;
+}
+
+.comment-text {
+  margin: 0 0 5px 0;
+}
+
+.comment-user {
+  display: block;
+  font-weight: bold;
+}
+
+.comment-date {
+  display: block;
+  font-size: 0.9em;
+  color: #888;
 }
 
 .add-comment {
   margin-top: 20px;
 }
 
-.add-comment input {
-  width: 70%;
+.comment-input {
+  width: calc(100% - 120px);
+  padding: 10px;
   margin-right: 10px;
-}
-
-.add-comment button {
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 5px 10px;
+  border: 1px solid #ccc;
   border-radius: 5px;
 }
 
-.add-comment button:hover {
+.comment-button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  border: none;
+  color: #fff;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.comment-button:hover {
   background-color: #0056b3;
+}
+
+.loading {
+  text-align: center;
+  padding: 20px;
 }
 </style>
